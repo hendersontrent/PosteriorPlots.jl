fe7444a474033557c442d7f8c3427276e468b5c0
Trent Henderson <trenthenderson@Trents-MacBook-Pro-2.local>
2021-07-18 19:55:47 +1000

    pkg dependency

--- src/PosteriorPlots.jl
+++ src/PosteriorPlots.jl
@@ -1,6 +1,6 @@
 module PosteriorPlots
 
-using DataFrames, Plots, StatsPlots, Random, Turing
+using DataFrames, Plots, StatsPlots, Random, KernelDensity, Turing
 
 include("plot_posterior_intervals.jl")
 include("plot_posterior_density_check.jl")
