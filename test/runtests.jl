25511c32d07ee65c8f0f3a72f473f44c14a3532a
Trent Henderson <trenthenderson@Trents-MacBook-Pro-2.local>
2021-07-26 11:08:38 +1000

    Better testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,19 +3,64 @@ using Test
 
 import Logging
 
-#------------- Build some demo models ---------------
-
-
-
-#------------- Run package tests --------------------
-
 # Silence all warnings
 
 level = Logging.min_enabled_level(Logging.current_logger())
 Logging.disable_logging(Logging.Warn)
 
+#------------- Build some demo models ---------------
+
+using Soss
+using Random
+
+model = @model X begin
+    p = size(X, 2) # number of features
+    α ~ Normal(0, 1) # intercept
+    β ~ Normal(0, 1) |> iid(p) # coefficients
+    σ ~ HalfNormal(1) # dispersion
+    η = α .+ X * β # linear predictor
+    μ = η # `μ = g⁻¹(η) = η`
+    y ~ For(eachindex(μ)) do j
+        Normal(μ[j], σ) # `Yᵢ ~ Normal(mean=μᵢ, variance=σ²)`
+    end
+end;
+
+X = randn(6,2)
+
+forward_sample = rand(model(X=X))
+
+num_rows = 1_000
+num_features = 2
+X = randn(num_rows, num_features)
+
+β_true = [2.0, -1.0]
+α_true = 1.0
+σ_true = 0.5
+
+η_true = α_true .+ X * β_true
+μ_true = η_true
+noise = randn(num_rows) .* σ_true
+y_true = μ_true .+ noise
+
+posterior = dynamicHMC(model(X=X), (y=y_true,))
+
+#------------- Run package tests --------------------
+
 @testset "PosteriorPlots.jl" begin
-    # Write your tests here.
+    # plot_posterior_intervals
+    println("plot_posterior_intervals")
+    plot(plot_posterior_intervals(posterior)...)
+    println()
+
+    # plot_posterior_hist
+    println("plot_posterior_hist")
+    plot(plot_posterior_hist(posterior, true)...)
+    println()
+
+    # plot_posterior_density
+    println("plot_posterior_density")
+    plot(plot_posterior_density(posterior, true)...)
+    println()
 end
 
 # Reset log level
