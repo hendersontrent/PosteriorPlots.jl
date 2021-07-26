a11cf510d680f8f461d2d6156affc827d69d1974
Trent Henderson <trenthenderson@Trents-MacBook-Pro-2.local>
2021-07-26 10:41:45 +1000

    Some tests structuring

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,23 @@
 using PosteriorPlots
 using Test
 
+import Logging
+
+#------------- Build some demo models ---------------
+
+
+
+#------------- Run package tests --------------------
+
+# Silence all warnings
+
+level = Logging.min_enabled_level(Logging.current_logger())
+Logging.disable_logging(Logging.Warn)
+
 @testset "PosteriorPlots.jl" begin
     # Write your tests here.
 end
+
+# Reset log level
+
+Logging.disable_logging(level)
