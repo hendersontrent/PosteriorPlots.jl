bb7428ec4717a343b5234c969fc835a3ecb7915e
Trent Henderson <trenthenderson@Trents-MacBook-Pro-2.local>
2021-07-27 19:43:05 +1000

    Started PPC dev

--- src/PosteriorPredictiveCheck.jl
+++ src/PosteriorPredictiveCheck.jl
@@ -1,42 +1,27 @@
 """
 
-    plot_density_check(model, predmodel, y, ndraws, args...; kwargs...)
+    plot_density_check(y, yrep, args...; kwargs...)
 
 Draw a density plot of a random sample of draws from the response variable posterior distribution against the density estimation of the actual data to visualise model fit.
 
 Usage:
 ```julia-repl
-plot_density_check(model, y, ndraws)
+plot_density_check(y, yrep)
 ```
 Arguments:
 
-- `model` : The `Turing.jl` or `Soss.jl` model of class `Chains` or `Array` to draw inferences from.
-- `predmodel` : The `Turing.jl` or `Soss.jl` model that specifies predictions for Missing input data.
 - `y` : The vector of response variable values.
-- `ndraws` : The number of random draws to make from the posterior distribution.
+- `yrep` : The Draws x Values matrix of posterior predictions.
 """
-function plot_density_check(model::Chains, predmodel::Model, y::Vector, ndraws::Int, args...; kwargs...)
-
-    #------------ Draws and plot ----------------
+function plot_density_check(y::Vector, yrep::AbstractMatrix, args...; kwargs...)
 
     gr() # gr backend for graphics
     mycolor = theme_palette(:auto).colors.colors[1]
     Random.seed!(123) # Fix seed for reproducibility
 
-    # Perform draws and add to plot
+    #-------- Draw plot --------
 
-    myPlot = plot()
-
-    for i in 1:ndraws
-        chain2 = sample(predmodel, NUTS(), 1000)
-        gen = generated_quantities(model, chain2)
-        plot!(p, predictions, label = string(ndraws, " Posterior Draws"), seriestype = :density, color = :grey, alpha = 0.5)
-    end
-
-    # Add original data to plot
-
-    plot!(myPlot, y, label = "Real Data", seriestype = :density, color = mycolor)
-    myPlot = plot!(myPlot, title = "Posterior Predictive Check", xlabel = "Value", ylabel = "Density")
+    x
 
     return myPlot
 end
@@ -45,37 +30,41 @@ end
 
 """
 
-    plot_hist_check(x, y, args...; kwargs...)
+    plot_hist_check(y, yrep, args...; kwargs...)
 
 Draw a plot with a binned histogram of posterior-predicted response variable values against a measure of centrality of the actual data to visualise model fit.
 
 Usage:
 ```julia-repl
-plot_hist_check(x, y)
+plot_hist_check(y, yrep)
 ```
 
 Arguments:
 
-- `x` : The vector of the actual data values to plot.
-- `y` : The vector of model response variable predictions to plot.
+- `y` : The vector of response variable values.
+- `yrep` : The Draws x Values matrix of posterior predictions.
+- `plot_legend` : Boolean of whether to add a legend to the plot or not.
 """
 
-function plot_hist_check(x::Vector, y::Vector, args...; kwargs...)
+function plot_hist_check(y::Vector, yrep::AbstractMatrix, plot_legend::Bool, args...; kwargs...)
 
-        #------------ Computations ------------------
+    # Compute median
 
-        m = median(x)
+    m = median(y)
 
-        #------------ Draw the plot -----------------
+    # Set up graphics helpers
 
-        gr() # gr backend for graphics
-        mycolor = theme_palette(:auto).colors.colors[1]
-        mycolor2 = theme_palette(:auto).colors.colors[2]
+    mycolor = theme_palette(:auto).colors.colors[1]
+    mycolor2 = theme_palette(:auto).colors.colors[2]
 
-        plot(x, label = "Real Data", seriestype = :histogram, color = mycolor)
-        plot!([m], seriestype = "vline", color = mycolor, label = "Real Data Median")
-        myPlot = plot!(title = "Posterior Predictive Check", xlabel = "Value", ylabel = "")
+    # Draw plot
 
-        return myPlot
+    myPlot = plot(yrep, seriestype = :histogram, fillalpha = 0.6, 
+                  xlabel = "Value", ylabel = "", label = "yrep",
+                  color = mycolor, title = "Posterior Predictive Check", size = (400, 400),
+                  legend = plot_legend)
 
+    plot!([m], seriestype = "vline", color = mycolor2, label = "y", linewidth = 2.5)
+
+    return myPlot
 end
